d394fee0babe12db0045fec8c26ee0d06aa45d35
서지유 <jihoseo2008@gmail.com>
2025-07-03 12:31:04 +0900

    feat :: design system

---
+++ src/constants/text.ts
@@ -0,0 +1,41 @@
+export const fontTable = {
+  heading: {
+    1: {
+      fontSize: 42,
+      fontFamily: 'SemiBold',
+    },
+    2: {
+      fontSize: 32,
+      fontFamily: 'SemiBold',
+    },
+    3: {
+      fontSize: 24,
+      fontFamily: 'SemiBold',
+    },
+  },
+  subTitle: {
+    1: {
+      fontSize: 24,
+      fontFamily: 'SemiBold',
+    },
+    2: {
+      fontSize: 18,
+      fontFamily: 'SemiBold',
+    },
+    3: {
+      fontSize: 16,
+      fontFamily: 'SemiBold',
+    },
+  },
+  body: {
+    1: {
+      fontSize: 16,
+      fontFamily: 'Medium',
+    },
+    2: {
+      fontSize: 14,
+      fontFamily: 'Medium',
+      lineHeight: 14,
+    },
+  },
+};
