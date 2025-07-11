97707047fe88347961478725050d08a4903370df
서지유 <jihoseo2008@gmail.com>
2025-07-11 14:11:59 +0900

    feat :: bottom bar component

--- App.tsx
+++ App.tsx
@@ -3,7 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { enableScreens } from 'react-native-screens';
-import { Login, Email, Role, Password } from './src/screens/index';
+import {
+  Login,
+  Email,
+  Role,
+  Password,
+  Game,
+  HouseDevice,
+  HouseSection,
+} from './src/screens/index';
 
 enableScreens();
 
@@ -21,6 +29,9 @@ function App() {
           <Stack.Screen name="Role" component={Role} />
           <Stack.Screen name="Email" component={Email} />
           <Stack.Screen name="Password" component={Password} />
+          <Stack.Screen name="HouseDevice" component={HouseDevice} />
+          <Stack.Screen name="HouseSection" component={HouseSection} />
+          <Stack.Screen name="Game" component={Game} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,6 @@ export * from './Login';
 export * from './register/Email';
 export * from './register/Password';
 export * from './register/Role';
+export * from './house/Device';
+export * from './house/Game';
+export * from './house/Section';
