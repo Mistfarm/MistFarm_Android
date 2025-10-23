499dd1eca51e138172bc627e8080b0f5f3c03896
서지유 <jihoseo2008@gmail.com>
2025-10-23 20:18:27 +0900

    feat :: testing components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import { useState } from "react"
+import Input from "./src/components/common/Input"
+import Button from "./src/components/common/Button"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [password, setPassword] = useState<string>("")
+
+    return (
+        <View style={styles.container}>
+            <Text>Op!</Text>
+
+            <Input
+                label="비밀번호"
+                placeholder="비밀번호를 입력하세요"
+                value={password}
+                onChange={setPassword}
+                password
+            />
+
+            <Button>main</Button>
+            <Button type="gray">gray</Button>
+            <Button type="error">error</Button>
+
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
