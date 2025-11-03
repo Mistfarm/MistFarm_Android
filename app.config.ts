23022b4e14e7f5c6e105fd0b40380c343e7d981b
서지유 <jihoseo2008@gmail.com>
2025-11-03 15:41:54 +0900

    fix :: update eas config and app.config.ts for EAS build

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,14 @@
 import "dotenv/config"
-import { ConfigContext, ExpoConfig } from "expo/config"
+import { ExpoConfig } from "@expo/config"
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-    ...config,
+const config: ExpoConfig = {
     name: "mistfarm",
     slug: "mistfarm",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    newArchEnabled: true,
     scheme: "mistfarm",
-
+    userInterfaceStyle: "light",
     splash: {
         image: "./assets/icon.png",
         resizeMode: "contain",
@@ -20,60 +17,37 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     assetBundlePatterns: ["**/*"],
     ios: {
         supportsTablet: true,
-        bundleIdentifier: "com.NoNamed.flp",
-        config: {
-            googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-        },
+        bundleIdentifier: "com.NoNamed.mistfarm",
     },
     android: {
         package: "com.NoNamed.mistfarm",
+        versionCode: 1,
         adaptiveIcon: {
             foregroundImage: "./assets/icon.png",
             backgroundColor: "#ffffff",
         },
-        permissions: ["INTERNET", "ACCESS_NETWORK_STATE"],
-        config: {
-            googleMaps: {
-                apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-            },
-        },
     },
-    web: {
-        favicon: "./assets/icon.png",
-    },
-    notification: {
-        icon: "./assets/icon.png",
-        color: "#6fce5e",
-        androidMode: "default",
-        androidCollapsedTitle: "mistfarm",
-    },
-    plugins: [
-        [
-            "expo-build-properties",
-            {
-                android: {
-                    enableProguardInReleaseBuilds: false,
-                    enableShrinkResourcesInReleaseBuilds: false,
-                    usesCleartextTraffic: true,
-                },
-            },
-        ],
-        [
-            "expo-notifications",
-            {
-                icon: "./assets/icon.png",
-                color: "#6fce5e",
-                defaultChannel: "default",
-            },
-        ],
-    ],
+    owner: "jihoseo",
     extra: {
         eas: {
             projectId: "0504388d-5170-48b2-a22b-2a522c663abb",
         },
         EXPO_PUBLIC_GOOGLE_MAPS_API_KEY:
             process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-        // EXPO_PUBLIC_BASE_URL: process.env.EXPO_PUBLIC_BASE_URL,
     },
-    owner: "jihoseo",
-})
+    plugins: [
+        "expo-build-properties",
+        [
+            "expo-build-properties",
+            {
+                android: {
+                    compileSdkVersion: 34,
+                    targetSdkVersion: 34,
+                    minSdkVersion: 23,
+                },
+            },
+        ],
+    ],
+}
+
+export default config
