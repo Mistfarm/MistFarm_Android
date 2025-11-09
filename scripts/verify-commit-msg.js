e7e13c5491e98ad6e07704f8bfda64f2a6df5ba9
서지유 <jihoseo2008@gmail.com>
2025-11-09 18:25:27 +0900

    feat :: husky

---
+++ scripts/verify-commit-msg.js
@@ -0,0 +1,26 @@
+import { readFileSync } from "fs"
+
+const commitMsgFilePath = process.argv[2]
+
+if (!commitMsgFilePath) {
+    console.error("✖ 커밋 메시지에 접근할 수 없습니다.")
+    process.exit(1)
+}
+
+const commitMessage = readFileSync(commitMsgFilePath, "utf8").trim()
+
+const validateCommitMessage = (commitMessage) => {
+    const [type, message] = commitMessage
+        .split(" :: ")
+        .map((token) => token.trim())
+    return (
+        ["feat", "fix", "chore", "style"].includes(type) &&
+        !message.includes(":")
+    )
+}
+
+const result = validateCommitMessage(commitMessage)
+if (result === 1) {
+    console.error("커밋양식을 지키시오 (feat,fix,chore) :: (msg)")
+}
+process.exit(result ? 0 : 1)
