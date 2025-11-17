e584b86ab97a80bf18dd9dad4a982a20e3c8ae99
서지유 <jihoseo2008@gmail.com>
2025-11-17 10:22:17 +0900

    feat :: axios, react-query, toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./styles/global.css"
+import "react-toastify/dist/ReactToastify.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Login, Mypage, Plant, Register, Plants, Setting, Start } from "./pages"
 import { Header } from "./components/common"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ToastContainer } from "react-toastify"
+
+const queryClient = new QueryClient()
 
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                pauseOnHover
+                closeOnClick
+            />
             <BrowserRouter>
                 <Header />
 
@@ -16,11 +28,10 @@ function App() {
                     <Route path="/mypage" element={<Mypage />} />
                     <Route path="/plants" element={<Plants />} />
                     <Route path="/setting" element={<Setting />} />
-                    {/* <Route path="/plants/:id" element={<Plant />} /> */}
                     <Route path="/detail" element={<Plant />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     )
 }
 
