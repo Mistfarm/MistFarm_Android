46264a2b4c30c7b8b9289e17f2819c0a89aee0b3
서지유 <jihoseo2008@gmail.com>
2025-11-19 11:02:23 +0900

    fix :: logout

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,65 @@ import { Login, Mypage, Plant, Register, Plants, Setting, Start } from "./pages"
 import { Header } from "./components/common"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ToastContainer } from "react-toastify"
+import { AuthProvider } from "./hooks/useAuth"
+import { ProtectedRoute } from "./components/common/ProtectedRoute"
 
 const queryClient = new QueryClient()
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <ToastContainer
-                position="top-right"
-                autoClose={2000}
-                hideProgressBar={false}
-                pauseOnHover
-                closeOnClick
-            />
-            <BrowserRouter>
-                <Header />
+            <AuthProvider>
+                <ToastContainer
+                    position="top-right"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    pauseOnHover
+                    closeOnClick
+                />
+                <BrowserRouter>
+                    <Header />
 
-                <Routes>
-                    <Route path="/" element={<Start />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/mypage" element={<Mypage />} />
-                    <Route path="/plants" element={<Plants />} />
-                    <Route path="/setting" element={<Setting />} />
-                    <Route path="/detail" element={<Plant />} />
-                </Routes>
-            </BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Start />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+
+                        <Route
+                            path="/mypage"
+                            element={
+                                <ProtectedRoute>
+                                    <Mypage />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/plants"
+                            element={
+                                <ProtectedRoute>
+                                    <Plants />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/setting"
+                            element={
+                                <ProtectedRoute>
+                                    <Setting />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/detail"
+                            element={
+                                <ProtectedRoute>
+                                    <Plant />
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
         </QueryClientProvider>
     )
 }
