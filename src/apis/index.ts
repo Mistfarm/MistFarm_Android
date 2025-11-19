62ee2ba2d71b48df02ce5f4a68a61a4a5fd023c4
서지유 <jihoseo2008@gmail.com>
2025-11-19 11:38:18 +0900

    fix :: get

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -13,7 +13,7 @@ instance.interceptors.request.use(
     (config: InternalAxiosRequestConfig) => {
         const token = tempCookie.getAccessToken()
         if (token) {
-            config.headers["Authorization"] = token
+            config.headers["Authorization"] = `Bearer ${token}`
         }
         return config
     },
