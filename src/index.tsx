c97652e348b371725b1e0551ff7ef5390f5c968b
서지유 <jihoseo2008@gmail.com>
2025-11-20 10:00:25 +0900

    feat :: delete import that don't use

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./styles/global.css"
 import App from "./App"
-import { AuthProvider } from "./hooks/useAuth"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
